29218fe7bbf11f352f44c50b64239dee059efeb7
Shristy1503 <shriimishra15@gmail.com>
2025-02-22 10:42:11 +0530

    [Shristy] Add.Path variable

--- src/main/java/com/bridgelabz/messagingApp/controller/UserController.java
+++ src/main/java/com/bridgelabz/messagingApp/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.messagingApp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,10 @@ public class UserController {
     @GetMapping("/hello/query")
     public String sayHello(@RequestParam String name){
         return "Hello " + name + " from BridgeLabz";
+    }
+    //in method pass name as path variable
+    @GetMapping("/hello/param/{name}")
+    public String display(@PathVariable("name") String name){
+        return "Hello " + name;
     }
 }
